1a71014bd181d266abe68916b5aecca46b7b1814
Renee Paczkowski <rpaczkowski@gmail.com>
2022-04-26 21:19:04 -0400

    Started writing code all over again

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-const name1 = "Ada Wong"
-const name2 = "Ash Williams"
-const name3 = "Leon S. Kennedy"
+let name1 = "Ada Wong"
+let name2 = "Ash Williams"
+let name3 = "Leon S. Kennedy"
 
-if (name1 > name2) {
-    console.log(`${name1} has the longest name.`)
-} else if (name2 > name1, name3) {
-    console.log(`${name2} has the longest name.`)
-} else if (name3 > name1, name2) {
-    console.log(`${name3} has the longest name.`)
-} else if (name1 === name2) {
-    
-}
