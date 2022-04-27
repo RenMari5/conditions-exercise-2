264db1ba868e77e65f06c3b3f19f2801dea0f177
Renee Paczkowski <rpaczkowski@gmail.com>
2022-04-26 21:29:58 -0400

    Commented out if else if and started switch

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ let name1 = "Ada Wong"
 let name2 = "Ash Williams"
 let name3 = "Leon S. Kennedy"
 
+// if (name1.length > name2.length) {
+//     console.log(`${name1} has the longest name.`)
+// } else if (name1.length > name3.length) {
+//      console.log(`${name1} has the longest name.`)
+// } else if (name2.length > name3.length) {
+//     console.log(`${name2} has the longest name.`)
+// } else if (name) {
+
+// } else if (name1.length === name2.length) {
+//     console.log()
+// }
+
+switch 
