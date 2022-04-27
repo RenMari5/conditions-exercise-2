e4c8c8e2b0cb34b59f182d5fb64b2897568cc162
Renee Paczkowski <rpaczkowski@gmail.com>
2022-04-26 21:56:37 -0400

    Got all but final comparison working

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ let name1 = "Ada Wong"
 let name2 = "Ash Williams"
 let name3 = "Leon S. Kennedy"
 
-// if (name1.length > name2.length) {
-//     console.log(`${name1} has the longest name.`)
-// } else if (name1.length > name3.length) {
-//      console.log(`${name1} has the longest name.`)
-// } else if (name2.length > name3.length) {
-//     console.log(`${name2} has the longest name.`)
-// } else if (name) {
-
-// } else if (name1.length === name2.length) {
-//     console.log()
-// }
-
-switch
+if (name1.length > name2.length && name1.length > name3.length) {
+    console.log(`${name1} has the longest name.`)
+} else if (name2.length > name1.length && name2.length > name3.length) {
+    console.log(`${name2} has the longest name.`)
+} else if (name3.length > name1.length && name3.length > name2.length) {
+    console.log(`${name3} has the longest name.`)
+} else if (name1.length === name2.length) {
+    console.log(`${name1} and ${name2} tie for the longest name.`)
+} else if (name1.length === name3.length) {
+     console.log(`${name1} and ${name3} tie for the longest name.`)
+} else if (name2.length === name3.length) {
+    console.log(`${name2} and ${name3} tie for the longest name.`)
+} else if (name1.length === name2.length && name1.length === name3.length && name2.length === name3.length) {
+    console.log(`All three names, ${name1}, ${name2}, and ${name3}, are the same length.`)
+}
